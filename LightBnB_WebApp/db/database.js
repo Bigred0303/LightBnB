b4db2ddaf6836b781f14ac8ff5d5172cf74c0a57
Grant <grantimaxi03@gmail.com>
2024-06-13 08:51:02 -0400

    Update addProperty to query version based off Larry feedback

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -130,11 +130,71 @@ const getAllProperties = (options, limit = 10) => {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = (property) => {
+  const {
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms,
+    country,
+    street,
+    city,
+    province,
+    post_code,
+    active
+  } = property;
+
+  const queryString = `
+    INSERT INTO properties (
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms,
+      country,
+      street,
+      city,
+      province,
+      post_code,
+      active
+    )
+    VALUES (
+      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15
+    )
+    RETURNING *;
+  `;
+
+  return pool.query(queryString, [
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms,
+    country,
+    street,
+    city,
+    province,
+    post_code,
+    active
+  ])
+    .then(res => res.rows[0])
+    .catch(err => {
+      console.error('query error', err.stack);
+      return null;
+    });
 };
 
 module.exports = {
